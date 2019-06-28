0d917f0181f0d56705e7f6cc92ec0879b1a83c67
Cheng Vue <csalvue@gmail.com>
2019-06-28 04:55:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,15 @@ function findMinAndRemoveSorted(array) {
 }
 
 function merge(arr1, arr2) {
+  let sortedArr = [];
 
+  while (arr1 !== 0 && arr2 !== 0) {
+    if (arr1[0] < arr2[0]) {
+      sortedArr.push(arr1[0])
+    } else {
+      sortedArr.psuh(arr2[0])
+    }
+  }
+
+  return sortedArr;
 }
